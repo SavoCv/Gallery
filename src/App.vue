40cfe5f377dcb99f4c9f97ba388e65df975de2dc
savocv <savo.cv2002@gmail.com>
2023-06-23 20:25:53 +0200

    Added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navigation></Navigation>
+    <router-view/>
+  </div>
 </template>
 
 <style>
+:root {
+  --lightest-green: #edf1d6;
+  --light-green: #9dc08b;
+  --dark-green: #609966;
+  --darkest-green: #40513b;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,3 +34,14 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation
+  }
+}
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,94 @@
+<template>
+    <nav class="navigation">
+        <div class="logo col-sm-1">
+            <img src="@/assets/logo.png" id="logo_img">
+        </div>
+        <ul class="nav-pills navigation col-sm-5">
+            <li class="nav-item"><router-link to="/" class="nav-link">Home</router-link></li>
+            <li class="nav-item"><router-link to="/artworks" class="nav-link">Artworks</router-link></li>
+            <li class="nav-item"><router-link to="/artists" class="nav-link">Artists</router-link></li>
+            <li class="nav-item"><router-link to="/account" class="nav-link">My account</router-link></li>
+            <li class="nav-item"><router-link to="/about" class="nav-link">About us</router-link></li>
+        </ul>
+        <div class="title offset-2 col-sm-2">
+            <h1><b>Gallery</b></h1>
+        </div>
+         <div class="nav-item offset-1 col-sm-1" id="flag">
+            <button @click="change_language()"> <!-- it's not working yet -->
+                <img src="@/assets/serbia_flag.png" id="flag">
+            </button>
+         </div>
+    </nav>
+</template>
+
+
+<style scoped>
+nav{
+    margin-left: 5%;
+    margin-right: 5%;
+}
+
+nav, ul{
+    height: 80px;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    display: flex;
+    /*justify-content: space-between;*/
+    align-items: center;
+    list-style-type: none;
+    background-color: var(--dark-green);
+}
+
+ul{
+    padding-top: 15px;
+}
+
+li:hover{
+    background-color: var(--lightest-green) !important;
+    border-radius: 10px;
+    color: white;
+    font-weight: bold;
+}
+
+a, a:hover, a:active{
+    text-decoration: none;
+    color: var(--darkest-green);
+}
+
+div{
+    display: flex;
+}
+
+#logo_img{
+    height: 80px;
+    width: 80px;
+}
+
+#flag{
+    width: 30px;
+}
+
+button{
+    border: none;
+    padding: 0;
+    background: transparent;
+}
+
+</style>
+
+<script>
+
+export default {
+    name: 'Navigation'
+}
+
+function change_language(){
+    console.log("nesto");
+    console.log(this.$router.currentRoute);
+}
+
+function say(message) {
+  alert(message)
+}
+
+</script>
