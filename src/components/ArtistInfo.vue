2968c9c5b35e99779e92bc4942093ada332a2559
SavoCv <savo.cv2002@gmail.com>
2023-06-26 16:02:06 +0200

    Added generating pdf for overview of artworks by some artist

--- src/components/ArtistInfo.vue
+++ src/components/ArtistInfo.vue
@@ -4,6 +4,9 @@
         <h1 class="mb-3" v-if="lang == 'srb'">{{artist.name_srb}}</h1>
         <span v-if="lang == 'uk'">{{ artist.info }}</span>
         <span v-if="lang == 'srb'">{{ artist.info_srb }}</span>
+        <br>
+        <button @click="overviewArts" v-if="lang == 'uk'">Overview of artworks</button>
+        <button @click="overviewArtsSrb" v-if="lang == 'srb'">Pregled dela</button>
     </div>
 </template>
 
@@ -16,6 +19,9 @@ div{
 </style>
 
 <script>
+import html2pdf from "html2pdf.js";
+import artworks from "../data/art-data.js";
+
 export default {
     name: 'ArtistInfo',
     data(){
@@ -26,5 +32,40 @@ export default {
     props: [
         'artist',
     ],
+    methods: {
+        makeOverview(heading, arts, name, a_d){
+            let htmlStr = "";
+            htmlStr += "<center><h1>" + heading + "</h1></center><br>";
+            htmlStr += "<ul>";
+            arts.forEach(element => {
+                htmlStr += "<li>";
+                htmlStr += element.name;
+                htmlStr += "</li>";
+            });
+            htmlStr += "</ul>";
+            html2pdf(htmlStr, {
+                margin: 5,
+                filename: name + " " + a_d + ".pdf",
+            });
+        },
+        overviewArts(){
+            let arts = artworks.map(a => {
+                return  {...a.eng, src: a.src};
+            });
+            arts = arts.filter(a => {
+                return a.author == this.artist.name;
+            })
+            this.makeOverview("List of artworks by the artist " + this.artist.name, arts, this.artist.name, "artworks");
+        },
+        overviewArtsSrb(){
+            let arts = artworks.map(a => {
+                return  {...a.srb, src: a.src};
+            });
+            arts = arts.filter(a => {
+                return a.author == this.artist.name_srb;
+            })
+            this.makeOverview("Lista dela od " + this.artist.name_srb, arts, this.artist.name_srb, "dela");
+        },
+    }
 }
 </script>
